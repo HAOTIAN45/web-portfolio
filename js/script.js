8ebf06f568719d98cce014129a21134cdffe31f5
HAOTIAN45 <danidwinugroho@gmail.com>
2024-05-24 14:42:25 +0700

    Cursor Animate

--- js/script.js
+++ js/script.js
@@ -12,3 +12,25 @@ document.addEventListener('click', function (e) {
         navBarNav.classList.remove('active');
     }
 })
+
+const cursorDot = document.querySelector("[data-cursor-dot]");
+const cursorOutline = document.querySelector("[data-cursor-outline]");
+
+window.addEventListener("mousemove", (e) => {
+    cursorDot.style.top = e.clientY + "px";
+    cursorDot.style.left = e.clientX + "px";
+    cursorDot.style.display = "block";
+    cursorOutline.style.display = "block";
+    // cursorOutline.style.top = e.pageY + "px";
+    // cursorOutline.style.left = e.pageX + "px";
+
+    cursorOutline.animate({
+        left: `${e.clientX}px`,
+        top: `${e.clientY}px`
+    }, { duration: 1000, fill: "forwards" })
+})
+
+document.addEventListener("mouseout", () => {
+    cursorDot.style.display = "none";
+    cursorOutline.style.display = "none";
+})
